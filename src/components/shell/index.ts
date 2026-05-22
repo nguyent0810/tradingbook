01fd06e3423eb72ae3cc64cb36c0dd68ef807f81
Sophia Green <sophiagreen966@fastmail.com>
2026-05-22 17:02:30 +0700

    Revert "feat(design): add reusable UI primitives and trading shell"
    This reverts commit 07bda1eb32d3a25c28d638143cf6475620952bad.

--- src/components/shell/index.ts
+++
@@ -1,5 +0,0 @@
-export { AppLogo } from "./app-logo";
-export { TopNav } from "./top-nav";
-export { MobileNav } from "./mobile-nav";
-export { TradingLayout } from "./trading-layout";
-export { PageHeader } from "./page-header";
